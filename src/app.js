379d4c5f9e9e4579e3fd56f85456e9fe923e91d0
Diego Mus <diegomus@Diego-Mus.local>
2024-01-01 21:27:42 -0600

    upload files

---
+++ src/app.js
@@ -0,0 +1,34 @@
+// app.js
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+require('dotenv').config();
+const prisma = new PrismaClient();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+  try{
+  const users = await prisma.user.findMany();
+  res.json(users);
+  } catch (error){
+    console.error('Error fetching users:', error);
+    res.status(500).send('Internal Server Erro');
+  }
+});
+
+app.post('/user', async (req, res) => {
+  const { name, lastname } = req.body;
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      lastname,
+    },
+  });
+  res.json(newUser);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
